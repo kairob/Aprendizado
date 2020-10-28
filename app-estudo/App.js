0a70ba201935e66a9acacdd717085adbadd751fe
kairob <kairob1234@gmail.com>
2020-10-28 18:52:49 -0300

    atualização

--- app-estudo/App.js
+++ app-estudo/App.js
@@ -1,4 +1,4 @@
-import { AppRegistry} from 'react-native';
-import App from "./index";
-import {name as appName} from "./app.json"
- AppRegistry.registerComponent(appName,()=> App);
+import{AppRegistry} from 'react-native';
+import App from './src/index';
+import { name as appName} from './app.json';
+AppRegistry.registerComponent(appName,()=> App);
---
+++ app-estudo/src/routes.js
@@ -0,0 +1,5 @@
+import {createStsackNavigator} from 'react-navigation';
+import Main from "./pages/main";
+export default createStsackNavigator ({
+  Main
+})
