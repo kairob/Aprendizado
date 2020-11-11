2720d9be06e40c98c99a46a9bcb947d10c7f1510
kairob <kairob1234@gmail.com>
2020-11-11 18:56:46 -0300

    novo

---
+++ Code/ReactNative/AprendaReactnatve/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {StyleSheet , View ,Text} from 'react-native';
+
+
+export default function App (){
+  render() {
+    return(
+      <View style ={style.container}>
+        <Text> </Text>
+        <Text> </Text>
+        </View>
+    );
+    
+  }
+}
--- app-estudo/src/routes.js
+++
@@ -1,5 +0,0 @@
-import {createStsackNavigator} from 'react-navigation';
-import Main from "./pages/main";
-export default createStsackNavigator ({
-  Main
-})
