b7636df98d189093f2550f0d80fef6525d397746
kairob <kairob1234@gmail.com>
2020-11-12 20:26:08 -0300

    atualização

--- Code/ReactNative/AprendaReactnatve/App.js
+++ Code/ReactNative/AprendaReactnatve/App.js
@@ -1,26 +1,13 @@
-import React, {Component }from 'react';
-import {StyleSheet , View ,Text} from 'react-native';
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
 
-
-
-
-export default function  App (){
-  render(){
-    return(
-      <><>
-
-  
-  
-    
-  );
-    
-    }}
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }),
-
-
+import styles from './src/Styles/index';
+export default function App () {
+  render() ; {
+    return (
+      <View style={styles.container}>
+        <text style={styles.texto}> Consegui ligar o Vs Code </text>
+        <Text style={styles.slogan}> E o emulador do Android</Text>
+      </View>,
+  }
+}
---
+++ Code/ReactNative/AprendaReactnatve/src/Styles/index.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+     backgroundColor: "#0066cc",
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    texto:{
+        color :'#000'
+    },
+    slogan:{
+        color :'#000'
+
+    }
+    });
+ export default styles;
