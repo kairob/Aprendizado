6354dc69fb7bdbfe90b12aa79c6326d300342c4f
kairob <kairob1234@gmail.com>
2021-04-07 09:25:45 -0300

    07042021

---
+++ ter_mo/src/Termo.html/script.js
@@ -0,0 +1,28 @@
+function calculate(){       
+  let iten=document.getElementById('txIten');
+  let iten1= document.getElementById('txIten1');
+  const res= document.getElementById('res')        
+  const quantityItems= parseInt(iten.value)
+  const quantityItemsAv= parseInt(iten1.value)
+  if(
+    quantityItemsAv > quantityItems
+     ||
+    quantityItemsAv + quantityItems === 0
+    
+    )
+    {
+    alert("Quantidade incorreta, Digite novamente")
+    res.innerHTML="Quantidade incorreta, Digite novamente"
+  }else{
+    const Result= (quantityItemsAv)/(quantityItems)*100
+    const Resultado=Math.round(Result)
+    const StringResult=(String(Resultado).replace(".",","))
+  res.innerHTML= `${StringResult} % dos itens estão avariados/danificados`;
+  console.log(quantityItems)
+  console.log(quantityItemsAv)
+  
+  console.log(StringResult)
+  }
+  
+} 
+   
